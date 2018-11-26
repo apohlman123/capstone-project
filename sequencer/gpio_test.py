2acc67091bd93e1c03fe2a380c2cd64764273579
AustinClay <apohlman@lion.lmu.edu>
2018-11-25 17:47:20 -0800

    Start button integrated

--- sequencer/gpio_test.py
+++ sequencer/gpio_test.py
@@ -27,106 +27,107 @@ BPM_secs = float(chosen_BPM)/60  #converts BPM to per second, rounds to whole
 BPM_delay = 1/BPM_secs         #converts to time delay in seconds
 
 #Wait for Start button press interrupt
-Start = GPIO.wait_for_edge(BStart, Rising, timeout = 5000)
+#Note: Start is False when triggered
+Start = GPIO.wait_for_edge(BStart, GPIO.RISING, timeout = 5000)
 
-if Start != True :
+if Start is None :
     print("Timed out waiting for start")
 else :
     print("Starting Sequencer!")
 
-#0
-GPIO.output(A3,False) ; GPIO.output(A2,False) ; GPIO.output(A1,False) ; GPIO.output(A0,False)
-print(current_sequence)
-time.sleep(round(BPM_delay,3))
+    #0
+    GPIO.output(A3,False) ; GPIO.output(A2,False) ; GPIO.output(A1,False) ; GPIO.output(A0,False)
+    print(current_sequence)
+    time.sleep(round(BPM_delay,3))
 
-#1
-GPIO.output(A3,False) ; GPIO.output(A2,False) ; GPIO.output(A1,False) ; GPIO.output(A0,True)
-current_sequence += 1
-print(current_sequence)
-time.sleep(round(BPM_delay,3))
+    #1
+    GPIO.output(A3,False) ; GPIO.output(A2,False) ; GPIO.output(A1,False) ; GPIO.output(A0,True)
+    current_sequence += 1
+    print(current_sequence)
+    time.sleep(round(BPM_delay,3))
 
-#2
-GPIO.output(A3,False) ; GPIO.output(A2,False) ; GPIO.output(A1,True) ; GPIO.output(A0,False)
-current_sequence += 1
-print(current_sequence)
-time.sleep(round(BPM_delay,3))
+    #2
+    GPIO.output(A3,False) ; GPIO.output(A2,False) ; GPIO.output(A1,True) ; GPIO.output(A0,False)
+    current_sequence += 1
+    print(current_sequence)
+    time.sleep(round(BPM_delay,3))
 
-#3
-GPIO.output(A3,False) ; GPIO.output(A2,False) ; GPIO.output(A1,True) ; GPIO.output(A0,True)
-current_sequence += 1
-print(current_sequence)
-time.sleep(round(BPM_delay,3))
+    #3
+    GPIO.output(A3,False) ; GPIO.output(A2,False) ; GPIO.output(A1,True) ; GPIO.output(A0,True)
+    current_sequence += 1
+    print(current_sequence)
+    time.sleep(round(BPM_delay,3))
 
-#4
-GPIO.output(A3,False) ; GPIO.output(A2,True) ; GPIO.output(A1,False) ; GPIO.output(A0,False)
-current_sequence += 1
-print(current_sequence)
-time.sleep(round(BPM_delay,3))
+    #4
+    GPIO.output(A3,False) ; GPIO.output(A2,True) ; GPIO.output(A1,False) ; GPIO.output(A0,False)
+    current_sequence += 1
+    print(current_sequence)
+    time.sleep(round(BPM_delay,3))
 
-#5
-GPIO.output(A3,False) ; GPIO.output(A2,True) ; GPIO.output(A1,False) ; GPIO.output(A0,True)
-current_sequence += 1
-print(current_sequence)
-time.sleep(round(BPM_delay,3))
+    #5
+    GPIO.output(A3,False) ; GPIO.output(A2,True) ; GPIO.output(A1,False) ; GPIO.output(A0,True)
+    current_sequence += 1
+    print(current_sequence)
+    time.sleep(round(BPM_delay,3))
 
-#6
-GPIO.output(A3,False) ; GPIO.output(A2,True) ; GPIO.output(A1,True) ; GPIO.output(A0,False)
-current_sequence += 1
-print(current_sequence)
-time.sleep(round(BPM_delay,3))
+    #6
+    GPIO.output(A3,False) ; GPIO.output(A2,True) ; GPIO.output(A1,True) ; GPIO.output(A0,False)
+    current_sequence += 1
+    print(current_sequence)
+    time.sleep(round(BPM_delay,3))
 
-#7
-GPIO.output(A3,False) ; GPIO.output(A2,True) ; GPIO.output(A1,True) ; GPIO.output(A0,True)
-current_sequence += 1
-print(current_sequence)
-time.sleep(round(BPM_delay,3))
+    #7
+    GPIO.output(A3,False) ; GPIO.output(A2,True) ; GPIO.output(A1,True) ; GPIO.output(A0,True)
+    current_sequence += 1
+    print(current_sequence)
+    time.sleep(round(BPM_delay,3))
 
-#8
-GPIO.output(A3,True) ; GPIO.output(A2,False) ; GPIO.output(A1,False) ; GPIO.output(A0,False)
-current_sequence += 1
-print(current_sequence)
-time.sleep(round(BPM_delay,3))
+    #8
+    GPIO.output(A3,True) ; GPIO.output(A2,False) ; GPIO.output(A1,False) ; GPIO.output(A0,False)
+    current_sequence += 1
+    print(current_sequence)
+    time.sleep(round(BPM_delay,3))
 
-#9
-GPIO.output(A3,True) ; GPIO.output(A2,False) ; GPIO.output(A1,False) ; GPIO.output(A0,True)
-current_sequence += 1
-print(current_sequence)
-time.sleep(round(BPM_delay,3))
+    #9
+    GPIO.output(A3,True) ; GPIO.output(A2,False) ; GPIO.output(A1,False) ; GPIO.output(A0,True)
+    current_sequence += 1
+    print(current_sequence)
+    time.sleep(round(BPM_delay,3))
 
-#10
-GPIO.output(A3,True) ; GPIO.output(A2,False) ; GPIO.output(A1,True) ; GPIO.output(A0,False)
-current_sequence += 1
-print(current_sequence)
-time.sleep(round(BPM_delay,3))
+    #10
+    GPIO.output(A3,True) ; GPIO.output(A2,False) ; GPIO.output(A1,True) ; GPIO.output(A0,False)
+    current_sequence += 1
+    print(current_sequence)
+    time.sleep(round(BPM_delay,3))
 
-#11
-GPIO.output(A3,True) ; GPIO.output(A2,False) ; GPIO.output(A1,True) ; GPIO.output(A0,True)
-current_sequence += 1
-print(current_sequence)
-time.sleep(round(BPM_delay,3))
+    #11
+    GPIO.output(A3,True) ; GPIO.output(A2,False) ; GPIO.output(A1,True) ; GPIO.output(A0,True)
+    current_sequence += 1
+    print(current_sequence)
+    time.sleep(round(BPM_delay,3))
 
-#12
-GPIO.output(A3,True) ; GPIO.output(A2,True) ; GPIO.output(A1,False) ; GPIO.output(A0,False)
-current_sequence += 1
-print(current_sequence)
-time.sleep(round(BPM_delay,3))
+    #12
+    GPIO.output(A3,True) ; GPIO.output(A2,True) ; GPIO.output(A1,False) ; GPIO.output(A0,False)
+    current_sequence += 1
+    print(current_sequence)
+    time.sleep(round(BPM_delay,3))
 
-#13
-GPIO.output(A3,True) ; GPIO.output(A2,True) ; GPIO.output(A1,False) ; GPIO.output(A0,True)
-current_sequence += 1
-print(current_sequence)
-time.sleep(round(BPM_delay,3))
+    #13
+    GPIO.output(A3,True) ; GPIO.output(A2,True) ; GPIO.output(A1,False) ; GPIO.output(A0,True)
+    current_sequence += 1
+    print(current_sequence)
+    time.sleep(round(BPM_delay,3))
 
-#14
-GPIO.output(A3,True) ; GPIO.output(A2,True) ; GPIO.output(A1,True) ; GPIO.output(A0,False)
-current_sequence += 1
-print(current_sequence)
-time.sleep(round(BPM_delay,3))
+    #14
+    GPIO.output(A3,True) ; GPIO.output(A2,True) ; GPIO.output(A1,True) ; GPIO.output(A0,False)
+    current_sequence += 1
+    print(current_sequence)
+    time.sleep(round(BPM_delay,3))
 
-#15
-GPIO.output(A3,True) ; GPIO.output(A2,True) ; GPIO.output(A1,True) ; GPIO.output(A0,True)
-current_sequence += 1
-print(current_sequence)
-time.sleep(round(BPM_delay,3))
+    #15
+    GPIO.output(A3,True) ; GPIO.output(A2,True) ; GPIO.output(A1,True) ; GPIO.output(A0,True)
+    current_sequence += 1
+    print(current_sequence)
+    time.sleep(round(BPM_delay,3))
 
 GPIO.cleanup()
